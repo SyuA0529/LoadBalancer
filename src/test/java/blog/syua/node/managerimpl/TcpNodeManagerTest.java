fe7baf99aa1cfca2fa386670760a219256e2700f
SyuA0529 <silbi3408@gmail.com>
2023-11-13 02:35:52 +0900

    test: TcpNodeManager 및 UdpNodeManager 테스트 작성

---
+++ src/test/java/blog/syua/node/managerimpl/TcpNodeManagerTest.java
@@ -0,0 +1,115 @@
+package blog.syua.node.managerimpl;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import blog.syua.node.nodeimpl.TcpNode;
+
+@DisplayName("TCP Node Manager 테스트")
+class TcpNodeManagerTest {
+
+	private static final int TEST_PORT = 10002;
+
+	private TcpNodeManager tcpNodeManager;
+
+	@BeforeEach
+	void beforeEach() throws IOException {
+		tcpNodeManager = new TcpNodeManager(TEST_PORT);
+	}
+
+	@Nested
+	@DisplayName("Method: startForward")
+	class MethodStartForward {
+		@Test
+		@DisplayName("등록된 UDP 노드로 포워딩을 수행한다")
+		void forwardToRegisteredUdpNode() throws IOException, InterruptedException {
+			//given
+			TcpNode mockedUdpNode = new TcpNode(InetAddress.getLocalHost().getHostAddress(), TEST_PORT) {
+				@Override
+				public void forwardPacket(Socket clientSocket) {
+					try {
+						OutputStream outputStream = clientSocket.getOutputStream();
+						outputStream.write("Hello".getBytes(StandardCharsets.UTF_8));
+						outputStream.close();
+						clientSocket.close();
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				}
+			};
+			tcpNodeManager.registerNode(mockedUdpNode);
+
+			//when
+			tcpNodeManager.startForwarding();
+			Thread.sleep(1000);
+			//then
+			assertThat(sendDataToNodeManager()).isEqualTo("Hello");
+		}
+	}
+
+	@Nested
+	@DisplayName("Method: unRegisterNode")
+	class MethodUnRegisterNode {
+		@Test
+		@DisplayName("UDP 노드가 존재하지 않을 경우 포워딩를 시작할 수 없다")
+		void cannotStartForward() throws UnknownHostException {
+			//given
+			TcpNode tcpNode = new TcpNode(InetAddress.getLocalHost().getHostAddress(), TEST_PORT);
+			tcpNodeManager.registerNode(tcpNode);
+
+			//when
+			tcpNodeManager.unRegisterNode(tcpNode);
+
+			//then
+			assertThatThrownBy(() -> tcpNodeManager.startForwarding())
+				.isInstanceOf(IllegalStateException.class)
+				.hasMessage("포워딩을 시작할 수 없습니다");
+		}
+
+		@Test
+		@DisplayName("TCP 노드가 존재하지 않는 경우 더이상 새로운 요청을 처리하지 않는다")
+		void doNotProcessNewRequest() throws InterruptedException, IOException {
+		    //given
+			TcpNode tcpNode = new TcpNode(InetAddress.getLocalHost().getHostAddress(), TEST_PORT);
+			tcpNodeManager.registerNode(tcpNode);
+			tcpNodeManager.startForwarding();
+			Thread.sleep(1000);
+
+			//when
+			tcpNodeManager.unRegisterNode(tcpNode);
+
+		    //then
+			assertThatThrownBy(TcpNodeManagerTest.this::sendDataToNodeManager)
+				.isInstanceOf(ConnectException.class)
+				.hasMessage("Connection refused: connect");
+		}
+	}
+
+	private String sendDataToNodeManager() throws IOException {
+		System.out.println("start sendData");
+		Socket clientSocket = new Socket(InetAddress.getLocalHost(), TEST_PORT);
+		OutputStream outputStream = clientSocket.getOutputStream();
+		outputStream.write("Client Data".getBytes(StandardCharsets.UTF_8));
+		outputStream.flush();
+		InputStream inputStream = clientSocket.getInputStream();
+		String result = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+		outputStream.close();
+		inputStream.close();
+		clientSocket.close();
+		return result;
+	}
+
+}
---
+++ src/test/java/blog/syua/node/managerimpl/UdpNodeManagerTest.java
@@ -0,0 +1,125 @@
+package blog.syua.node.managerimpl;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import blog.syua.node.nodeimpl.UdpNode;
+
+@DisplayName("UDP Node Manager 테스트")
+class UdpNodeManagerTest {
+
+	private static final int TEST_PORT = 10003;
+
+	private UdpNodeManager udpNodeManager;
+
+	@BeforeEach
+	void beforeEach() throws SocketException {
+		udpNodeManager = new UdpNodeManager(TEST_PORT);
+	}
+
+	@Nested
+	@DisplayName("Method: startForward")
+	class MethodStartForward {
+		@Test
+		@DisplayName("등록된 UDP 노드로 포워딩을 수행한다")
+		void forwardToRegisteredUdpNode() throws IOException {
+			//given
+			UdpNode mockedUdpNode = new UdpNode(InetAddress.getLocalHost().getHostAddress(), TEST_PORT) {
+				@Override
+				public void forwardPacket(DatagramSocket loadBalancerSocket, DatagramPacket clientPacket) {
+					try {
+						byte[] data = "Hello".getBytes(StandardCharsets.UTF_8);
+						DatagramPacket packet = new DatagramPacket(data, data.length, clientPacket.getAddress(),
+							clientPacket.getPort());
+						loadBalancerSocket.send(packet);
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				}
+			};
+			udpNodeManager.registerNode(mockedUdpNode);
+
+			//when
+			udpNodeManager.startForwarding();
+			String result = sendDataToNodeManager();
+
+			//then
+			assertThat(result).isEqualTo("Hello");
+		}
+	}
+
+	@Nested
+	@DisplayName("Method: unRegisterNode")
+	class MethodUnRegisterNode {
+		@Test
+		@DisplayName("UDP 노드가 존재하지 않을 경우 포워딩을 시작할 수 없다")
+		void cannotStartForwarding() throws UnknownHostException {
+			//given
+			UdpNode udpNode = new UdpNode(InetAddress.getLocalHost().getHostAddress(), 0);
+			udpNodeManager.registerNode(udpNode);
+
+			//when
+			udpNodeManager.unRegisterNode(udpNode);
+
+			//then
+			assertThatThrownBy(() -> udpNodeManager.startForwarding())
+				.isInstanceOf(IllegalStateException.class)
+				.hasMessage("포워딩을 시작할 수 없습니다");
+		}
+
+		@Test
+		@DisplayName("UDP 노드가 존재하지 않는 경우 더이상 새로운 요청을 처리하지 않는다")
+		void doNotProcessNewRequest() throws InterruptedException, IOException {
+			//given
+			UdpNode udpNode = new UdpNode(InetAddress.getLocalHost().getHostAddress(), TEST_PORT);
+			udpNodeManager.registerNode(udpNode);
+			udpNodeManager.startForwarding();
+			Thread.sleep(1000);
+
+			//when
+			udpNodeManager.unRegisterNode(udpNode);
+
+			//then
+			assertThatThrownBy(UdpNodeManagerTest::sendDataToNodeManager)
+				.isInstanceOf(SocketTimeoutException.class)
+				.hasMessage("Receive timed out");
+		}
+	}
+
+	private static String sendDataToNodeManager() throws IOException {
+		byte[] data = "client data".getBytes(StandardCharsets.UTF_8);
+		DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getLocalHost(), TEST_PORT);
+		DatagramSocket socket = new DatagramSocket();
+		socket.setSoTimeout(3000);
+		socket.send(packet);
+
+		DatagramPacket resultPacket = new DatagramPacket(new byte[socket.getReceiveBufferSize()],
+			socket.getReceiveBufferSize());
+		socket.receive(resultPacket);
+		socket.close();
+		return new String(removeTrailingZeros(resultPacket.getData()), StandardCharsets.UTF_8);
+	}
+
+	private static byte[] removeTrailingZeros(byte[] data) {
+		int endIndex = data.length - 1;
+		while (endIndex >= 0 && data[endIndex] == 0) {
+			endIndex--;
+		}
+		return Arrays.copyOf(data, endIndex + 1);
+	}
+
+}
