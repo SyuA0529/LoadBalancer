f14e814a486956f6d0304609400cc9828958ffe5
SyuA0529 <silbi3408@gmail.com>
2023-11-13 21:06:32 +0900

    feat: 누락된 ForwardInfo 커밋

---
+++ src/main/java/blog/syua/forward/ForwardInfo.java
@@ -0,0 +1,31 @@
+package blog.syua.forward;
+
+import blog.syua.node.Node;
+import blog.syua.node.Protocol;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public class ForwardInfo {
+
+	private final Protocol protocol;
+	private final int port;
+
+	private ForwardInfo(Protocol protocol, int port) {
+		this.protocol = protocol;
+		this.port = port;
+	}
+
+	private ForwardInfo(Node node) {
+		this.protocol = node.getProtocol();
+		this.port = node.getPort();
+	}
+
+	public static ForwardInfo of(Protocol protocol, int port) {
+		return new ForwardInfo(protocol, port);
+	}
+
+	public static ForwardInfo of(Node node) {
+		return new ForwardInfo(node);
+	}
+
+}
