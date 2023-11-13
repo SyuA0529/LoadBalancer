cec049da946952331e0a87409cc46c8652c66155
SyuA0529 <silbi3408@gmail.com>
2023-11-13 21:15:48 +0900

    feat: 정상적인 socket interrupt 예외의 메세지를 반환하는 메소드 추가

--- src/main/java/blog/syua/utils/NodeMessageUtil.java
+++ src/main/java/blog/syua/utils/NodeMessageUtil.java
@@ -2,11 +2,25 @@ package blog.syua.utils;
 
 import java.nio.charset.StandardCharsets;
 
+import blog.syua.node.Protocol;
+
 public class NodeMessageUtil {
 
+	private static final String TCP_SOCKET_INTERRUPT_MESSAGE = "Interrupted function call: accept failed";
+	private static final String UDP_SOCKET_INTERRUPT_MESSAGE = "socket closed";
 	private static final String FORWARD_ERROR_MESSAGE = "패킷 포워딩에 실패하였습니다";
 
 	private NodeMessageUtil() {
+	}
+
+	public static String getSocketInterruptMessage(Protocol protocol) {
+		if (protocol.equals(Protocol.TCP)) {
+			return TCP_SOCKET_INTERRUPT_MESSAGE;
+		}
+		if (protocol.equals(Protocol.UDP)) {
+			return UDP_SOCKET_INTERRUPT_MESSAGE;
+		}
+		return null;
 	}
 
 	public static byte[] getForwardErrorMessage() {
